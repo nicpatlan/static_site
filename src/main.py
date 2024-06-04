5df7cf9dbe60f61280e2bb1343315239c5c48318
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-06-04 11:12:30 -0700

    added recursive function to convert nested markdown files into html

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 from copydir import copy_directory_to_directory
-from static_generate import generate_page
+from static_generate import generate_pages_recursively
 
 source_dir = "static"
 destination_dir = "public"
 
-from_path = "content/index.md"
+from_path = "content"
 template_path = "template.html"
-dest_path = "public/index.html"
+dest_path = "public"
 
 
 def main():
     copy_directory_to_directory(source_dir, destination_dir)
-    generate_page(from_path, template_path, dest_path)
+    generate_pages_recursively(from_path, template_path, dest_path)
 
 main()
--- src/static_generate.py
+++ src/static_generate.py
@@ -1,4 +1,4 @@
-import os
+import os, pathlib
 from block_to_html import markdown_to_html_node
 from htmlnode import HtmlNode
 
@@ -32,7 +32,30 @@ def generate_page(from_path, template_path, dest_path):
 
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
-        os.mkdirs(dest_path)
+        os.makedirs(dest_dir)
 
     with open(dest_path, "w") as f:
         f.write(static_html)
+
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+    if os.path.exists(dir_path_content):
+        #if not os.path.exists(dest_dir_path):
+        #    print(f'Creating new directory at {dest_dir_path}')
+        #    os.mkdir(dest_dir_path)
+        print(f'Checking directory at {dir_path_content} for markdown files')
+        list_dir = os.listdir(dir_path_content)
+
+        for element in list_dir:
+            new_path = pathlib.Path(dir_path_content, element)
+            if os.path.isfile(new_path):
+                if element.endswith(".md"):
+                    new_ext_name = element[:len(element) - 2] + "html"
+                    new_dest_path = pathlib.Path(dest_dir_path, new_ext_name)
+                    print(f'Converting {element} to html at {new_dest_path}')
+                    generate_page(new_path, template_path, new_dest_path)
+            else:
+                new_dest_path = pathlib.Path(dest_dir_path, element)
+                print(f'Recursively checking for markdown files at {new_dest_path}')
+                generate_pages_recursively(new_path, template_path, new_dest_path)
+    else:
+        raise Exception("invalid directory path")
